366283cd7476e73e306047d81231734308e60ec6
CristinaPineda <cristinadaspineda@yahoo.com.br>
2025-09-21 00:13:58 -0300

    :bug: retira data do glue

--- main.tf
+++ main.tf
@@ -7,10 +7,6 @@ data "aws_s3_bucket" "data_output" {
   bucket = var.data_output_bucket_name
 }
 
-data "aws_glue_job" "glue_job" {
-  name = var.glue_job_name
-}
-
 module "lambda_service" {
   source = "./modules/lambda_service"
 
@@ -22,10 +18,10 @@ module "lambda_service" {
   batch_size               = var.batch_size
   aws_statefile_s3_bucket  = var.aws_statefile_s3_bucket
   aws_lock_dynamodb_table  = var.aws_lock_dynamodb_table
+  glue_job_name            = var.glue_job_name
   
   sqs_queue_name           = data.aws_sqs_queue.source_queue.name
   sqs_queue_arn            = data.aws_sqs_queue.source_queue.arn
-  glue_job_name            = data.aws_glue_job.glue_job.name
   data_output_bucket_name  = data.aws_s3_bucket.data_output.bucket
 
   s3_bucket_name           = var.s3_bucket_name
